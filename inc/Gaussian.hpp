89c36f02b0c0e0c20d8dde897fcd9233a269d183
Clovis Durand <cd.clovel19@gmail.com>
2019-08-23 14:10:42 +0200

    Cleaned up source code

--- inc/Gaussian.hpp
+++ inc/Gaussian.hpp
@@ -1,71 +1,58 @@
-// // Clovis Durand
-// ENSEIRB-Matmeca - LASSENA (ETS)
-// 12/06/2017
-
-// Gaussian calculation on n points
-/*
+/* @brief Gaussian calculation on n points
  * This Class calculates n points of a gaussian bell
- * and stocks the info in a vector as it claculates them
+ * and stores the info in a vector as it claculates them
  */
 
 #ifndef GAUSSIAN_H
 #define GAUSSIAN_H
 
-// Includes ---------------------------------------------------------
+/* Includes -------------------------------------------- */
 
-#include <iostream>     // To print to stdout
-#include <fstream>      // To print in a file
-#include <vector>       // For main vector attribute
-#include <string>       // For strings and filenames
-#include <cmath>
+/* Defines --------------------------------------------- */
 
-// Defines ----------------------------------------------------------
+#define PI 3.14159265359f   /* Definition of the approximate pi number */
 
-typedef unsigned int uInt;
-
-#define PI 3.14159265359f   // Definition of the approximate pi number
-
-// Classes ----------------------------------------------------------
+/* Classes --------------------------------------------- */
 class Gaussian
 {
     public:
-        // Con/Destructor
-        Gaussian(const int &samplerate,
-                 const float &amplitude = 1,
-                 const float &stddev = 1.48631f *(float)pow(10, -6),
-                 const float &mean = 0);
+        /* Con/Destructor */
+        Gaussian(const int &pSamplerate,
+                 const float &pAmplitude    = 1.0f,
+                 const float &pStddev       = 1.48631f * (float)pow(10, -6),
+                 const float &pMean         = 0.0f);
         ~Gaussian();
 
-        // Getters
-        uInt                getNbPoints(void) const;
+        /* Getters */
+        unsigned int        getNbPoints(void) const;
         std::vector<float>  getPointVector(void) const;
-        float               getPointAtIndex(const uInt &index) const;
+        float               getPointAtIndex(const unsigned int &pIndex) const;
 
         float   getAmplitude(void) const;
         float   getMean(void) const;
         float   getStddev(void) const;
         float   getSamplerate(void) const;
 
-        // Setters : None because is fixed object.
+        /* Setters : None because it is a constant object. */
 
-        void    addSample(const uInt &index, const float &element);
-        void    addSample(const float &element);
+        /* Modifiers */
+        void    addSample(const unsigned int &pIndex, const float &pElement);
+        void    addSample(const float &pElement);
 
-        // Printers & File Management
-
-        void    printPoint(const uInt &index) const;
+        /* Printers & File Management */
+        void    printPoint(const unsigned int &pIndex) const;
         void    printPointVector(void) const;
-        void    VectorToFile(const std::string &filename);
+        void    vectorToFile(const std::string &pFilename);
 
-    private:
-        std::vector<float> m_points;
+    protected:
+        std::vector<float> mPoints;
 
-        float m_amplitude;  // Maximum of the bell / Amplitude of the Gaussian bell
-        float m_stddev;     // Standard deviation (ou écart type en francais), symbolized in math by "sigma"
-        float m_mean;       // Mean/Expected value, symbolized in math by "µ". Is 0 as default
-        int m_samplerate;   // Sample Rate with wich we be be calculating our interval of sample
+        float mAmplitude;   // Maximum of the bell / Amplitude of the Gaussian bell
+        float mStddev;      // Standard deviation (ou écart type en francais), symbolized in math by "sigma"
+        float mMean;        // Mean/Expected value, symbolized in math by "µ". Is 0 as default
+        int mSampleRate;    // Sample Rate with wich we be be calculating our interval of sample
 
-        float   gaussFunction(const float &x);
+        float   gaussFunction(const float &pX);
         void    calculate_points(void);
 };
 
--- src/Gaussian.cpp
+++ src/Gaussian.cpp
@@ -1,146 +1,144 @@
-// Clovis Durand
-// ENSEIRB-Matmeca - LASSENA (ETS)
-// 12/06/2017
-
-// Gaussian calculation on n points
-/*
+/* @brief Gaussian calculation on n points
  * This Class calculates n points of a gaussian bell
- * and stocks the info in a vector as it claculates them
+ * and stores the info in a std::vector as it claculates them
  */
 
-// Includes ---------------------------------------------------------
-
+/* Includes -------------------------------------------- */
+/* Gaussian */
 #include "Gaussian.hpp"         // Header file for Gaussian class
 
-// Namespace --------------------------------------------------------
-using namespace std;
+/* System */
+#include <iostream>     /* To print to stdout */
+#include <fstream>      /* To print in a file */
+#include <vector>       /* For main vector attribute */
+#include <string>       /* For strings and filenames */
+#include <cmath>
 
-// Con/Destructors --------------------------------------------------
-
-Gaussian::Gaussian(const int    &samplerate,
-                   const float  &amplitude,
-                   const float  &stddev,
-                   const float  &mean) :
-    m_samplerate(samplerate),
-    m_amplitude(amplitude),
-    m_stddev(stddev),
-    m_mean(mean)
+/* Class implementation */
+/* Con/Destructors */
+Gaussian::Gaussian(const int    &pSamplerate,
+                   const float  &pAmplitude,
+                   const float  &pStddev,
+                   const float  &pMean) :
+    mSampleRate(pSamplerate),
+    mAmplitude(pAmplitude),
+    mStddev(pStddev),
+    mMean(pMean)
 {
-    // Default constructor
+    /* Default constructor */
     calculate_points();
 }
 
 Gaussian::~Gaussian()
 {
-    // Default Destructor
+    /* Default Destructor */
 }
 
-// Getters ----------------------------------------------------------
-
-uInt Gaussian::getNbPoints(void) const
+/* Getters */
+unsigned int Gaussian::getNbPoints(void) const
 {
-    return m_points.size();
+    return mPoints.size();
 }
 
-vector<float> Gaussian::getPointVector(void) const
+std::vector<float> Gaussian::getPointVector(void) const
 {
-    return m_points;
+    return mPoints;
 }
 
-float Gaussian::getPointAtIndex(const uInt &index) const
+float Gaussian::getPointAtIndex(const unsigned int &pIndex) const
 {
-    if (index < getNbPoints()) {
-        return m_points[index];
+    if (pIndex < getNbPoints()) {
+        return mPoints[pIndex];
     } else {
-        return 0.0f;
+        return .0f;
     }
 }
 
 float Gaussian::getAmplitude(void) const
 {
-    return m_amplitude;
+    return mAmplitude;
 }
 
 float Gaussian::getMean(void) const
 {
-    return m_mean;
+    return mMean;
 }
 
 float Gaussian::getStddev(void) const
 {
-    return m_stddev;
+    return mStddev;
 }
 
 float Gaussian::getSamplerate(void) const
 {
-    return m_samplerate;
+    return mSampleRate;
 }
 
-// Setters/Mutators -------------------------------------------------
+/* Setters/Mutators */
+/* Once an attribute is changed you need to recalculate
+ * all the points.
+ *
+ * BEWARE : These functions do not recalculate all the points
+ */
 
-// Once an attribute is change you need to recalculate
-// all the points.
-
-// BEWARE : These functions do not recalculate all the points
-
-void Gaussian::addSample(const uInt &index, const float &element)
+void Gaussian::addSample(const unsigned int &pIndex, const float &pElement)
 {
-    m_points.insert(m_points.begin() + index, element);
+    mPoints.insert(mPoints.begin() + pIndex, pElement);
 }
 
-void Gaussian::addSample(const float &element)
+void Gaussian::addSample(const float &pElement)
 {
-    m_points.push_back(element);
+    mPoints.push_back(pElement);
 }
 
-// Printers ---------------------------------------------------------
-
-void Gaussian::printPoint(const uInt &index) const
+/* Printers */
+void Gaussian::printPoint(const unsigned int &pIndex) const
 {
-    cout << "g[" << index << "] = " << getPointAtIndex(index);
+    std::cout << "g[" << pIndex << "] = " << getPointAtIndex(pIndex);
 }
 
 void Gaussian::printPointVector(void) const
 {
-    cout << "\n\n----------------------------------\n\n";
-    cout << "Printing Gaussian Vector values : \n\n";
-    for (uInt i = 0; i < getNbPoints() - 1; i++) {
+    std::cout   << std::endl << std::endl
+                << "----------------------------------" << std::endl << std::endl;
+    std::cout << "Printing Gaussian std::vector values : " << std::endl << std::endl;
+
+    for (unsigned int i = 0U; i < getNbPoints() - 1; ++i) {
         printPoint(i);
-        cout << ", \n";
+        std::cout << ", " << std::endl;
     }
     printPoint(getNbPoints() - 1);
-    cout << endl;
+    std::cout << std::endl;
 }
 
-void Gaussian::VectorToFile(const string &filename)
+void Gaussian::vectorToFile(const std::string &pFilename)
 {
-    ofstream flux(filename);
+    std::ofstream lFlux(pFilename);
 
-    if (flux) {
-        string line;
-        for (uInt i = 0; i < getNbPoints(); i++) {
-            line    = to_string(getPointAtIndex(i));
-            line    += "\n";
+    if (lFlux) {
+        std::string lLine;
+        for (unsigned int i = 0U; i < getNbPoints(); ++i) {
+            lLine = std::to_string(getPointAtIndex(i));
 
-            flux << line;
+            lFlux << lLine << std::endl;
         }
-        flux.close();
+        lFlux.close();
     } else {
-        cerr << "[ERROR]  : Could not open file [" << filename << "]\n";
+        std::cerr << "[ERROR] Could not open file [" << pFilename << "]" << std::endl;
     }
 }
 
-// Misc. Private methods --------------------------------------------
-
-float Gaussian::gaussFunction(const float &x)
+//* Misc. Private methods */
+float Gaussian::gaussFunction(const float &pX)
 {
-    return getAmplitude() * exp(-pow(x - getMean(), 2) / (2 * pow(getStddev(), 2)));
+    return getAmplitude() * exp(-pow(pX - getMean(), 2) / (2 * pow(getStddev(), 2)));
 }
 
 void Gaussian::calculate_points(void)
 {
-    // Here goes the algorithm that calculated the points
-    // and adds them to the vector
+    /* Here goes the algorithm that calculated the points
+     * and adds them to the :vector
+     */
 
     /*
      * Lets recall that a Gaussian function is of the form :
@@ -158,43 +156,41 @@ void Gaussian::calculate_points(void)
      *
      * For a 3.5 us interval in between the two 50% of amp points
      * we need a +1.48631*10^6 stddev. This will be our default value
-     * because of LASSENA R.A.S. Project DME modulas
      */
 
-    float   element = 0;
-    int     i       = 0;        // x in the math fomula
+    float   lElement = .0f;
+    int     i       = 0;        /* x in the math fomula */
 
-    float interval = 1.0 / (float)getSamplerate();
+    float interval = 1.0f / (float)getSamplerate();
 
-    addSample(0, getAmplitude()); // On ajoute le premier element
+    addSample(0, getAmplitude()); /* Adding the first lElement */
 
     do {
-        i++;
-        element = gaussFunction(i * interval);
+        lElement = gaussFunction(++i * interval);
 
-        // Adding right side value
-        if (element >= 0.01f) {
-            addSample(0, element);
+        /* Adding right side value */
+        if (.01f <= lElement) {
+            addSample(0U, lElement);
         }
 
-        // Here, nb_pts = 2*i
+        /* Here, nb_pts = 2*i */
 
-        // Adding left side value
-        //addSample(0, element);
+        /* Adding left side value */
+        //addSample(0, lElement);
 
-        // Here, nb_pts = 2*i + 1
-    } while (element >= 0.01f);
+        /* Here, nb_pts = 2*i + 1 */
+    } while (.01f <= lElement);
 
     // Test
-    /*cout << "\nElement at index = agetNbPoints() - 1 is " << getPointAtIndex(getNbPoints() - 1) << endl;
+    /*std::cout << "\nElement at index = agetNbPoints() - 1 is " << getPointAtIndex(getNbPoints() - 1) << endl;
      * addSample(getPointAtIndex(getNbPoints() - 2 - 0));
-     * cout << "\nElement at index = agetNbPoints() - 1 is " << getPointAtIndex(getNbPoints() - 1) << endl;
+     * std::cout << "\nElement at index = agetNbPoints() - 1 is " << getPointAtIndex(getNbPoints() - 1) << endl;
      * addSample(getPointAtIndex(getNbPoints() - 4 - 0));
-     * cout << "\nElement at index = agetNbPoints() - 1 is " << getPointAtIndex(getNbPoints() - 1) << endl;
+     * std::cout << "\nElement at index = agetNbPoints() - 1 is " << getPointAtIndex(getNbPoints() - 1) << endl;
      * addSample(getPointAtIndex(getNbPoints() - 6 - 0));
-     * cout << "\nElement at index = agetNbPoints() - 1 is " << getPointAtIndex(getNbPoints() - 1) << endl;*/
+     * std::cout << "\nElement at index = agetNbPoints() - 1 is " << getPointAtIndex(getNbPoints() - 1) << endl;*/
 
-    for (uInt j = 2; j < getNbPoints() - 1; j += 2) {
+    for (unsigned int j = 2U; j < getNbPoints() - 1; j += 2) {
         addSample(getPointAtIndex(getNbPoints() - j));
     }
 }
